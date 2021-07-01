d9d98d4bdc1ed201b947caaf85d67774af57030c
naksh1123 <46441669+naksh1123@users.noreply.github.com>
2021-07-01 12:55:13 +0530

    With useEffect

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import "./styles.css";
 import React, { Component } from "react";
 
 import AsyncSelect from "react-select/async";
-import { stateOptions } from "./data";
 
-const filterColors = (inputValue: string) => {
-  return stateOptions.filter((i) =>
-    i.label.toLowerCase().includes(inputValue.toLowerCase())
-  );
-};
-
-const promiseOptions = (inputValue) =>
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(filterColors(inputValue));
-    }, 1000);
-  });
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [stateOptions, setStateOptions] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://mocki.io/v1/89f2a03f-4323-4d31-a3e8-bf88d2989e1c`)
+      .then((res) => res.json())
+      .then((data) => setStateOptions(data));
+  });
+
+  const filterState = (inputValue: string) => {
+    return stateOptions.filter((i) =>
+      i.label.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  };
+
+  const promiseOptions = (inputValue) =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(filterState(inputValue));
+        console.log(filterState(inputValue));
+      }, 1000);
+    });
+
   return (
     <div className="App">
       <AsyncSelect cacheOptions defaultOptions loadOptions={promiseOptions} />
