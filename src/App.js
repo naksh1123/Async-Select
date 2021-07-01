dc32e36372598a2354846603687b25054dfb4ed7
naksh1123 <46441669+naksh1123@users.noreply.github.com>
2021-07-01 13:08:06 +0530

    SetValue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import React, { Component } from "react";
+import React from "react";
 
 import AsyncSelect from "react-select/async";
 
@@ -7,6 +7,7 @@ import { useState, useEffect } from "react";
 
 export default function App() {
   const [stateOptions, setStateOptions] = useState([]);
+  const [values, setValues] = useState({});
 
   useEffect(() => {
     fetch(`https://mocki.io/v1/89f2a03f-4323-4d31-a3e8-bf88d2989e1c`)
@@ -24,13 +25,23 @@ export default function App() {
     new Promise((resolve) => {
       setTimeout(() => {
         resolve(filterState(inputValue));
-        console.log(filterState(inputValue));
       }, 1000);
     });
 
+  const handlerequest = (e) => {
+    console.log(e.value);
+    setValues(e);
+  };
+
   return (
     <div className="App">
-      <AsyncSelect cacheOptions defaultOptions loadOptions={promiseOptions} />
+      <AsyncSelect
+        cacheOptions
+        defaultOptions
+        loadOptions={promiseOptions}
+        onChange={handlerequest}
+        value={values}
+      />
     </div>
   );
 }
